0206c378fff509b804d81285a2186afbce47825b
Sudarshan <sudarshan.dhatrak@gmail.com>
2024-01-04 12:49:45 +0530

    Handling transactions during wallet creation to maintain consistency

--- service/wallet.js
+++ service/wallet.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import utilityConstants from "../constants/constants.js";
 import { createTransactionData } from "../odm/transaction.js";
 import { getWalletData, createWalletData } from "../odm/wallet.js";
@@ -18,21 +19,28 @@ export const getWallet = async (req, res) => {
 
 
 export const createWallet = async (req, res) => {
+    const session = await mongoose.startSession();
     try {
         logger.info('wallet@createWallet');
-        const walletData = await createWalletData({name: req.body.name, balance: req.body.amount})
+        session.startTransaction(); // Handling transactions to maintain consistancy
+        const walletData = await createWalletData({ name: req.body.name, balance: req.body.amount }, { session })
         const transactionData = await createTransactionData({
             walletId: walletData._id,
             description: utilityConstants.enums.initialTransaction,
             amount: walletData.balance,
             newBalance: walletData.balance,
             type: utilityConstants.enums.transactionTypeObject.credit
-        });
+        }, { session });
+
+        await session.commitTransaction();
         res.status(utilityConstants.serviceResponseCodes.success).json({ _id: walletData._id, balance: walletData.balance, transactionId: transactionData._id, name: walletData.name, date: walletData.createdAt })
     } catch (error) {
         logger.error(error);
         res
             .status(utilityConstants.serviceResponseCodes.serverError)
             .json({ error: utilityConstants.commonResponse.serverError });
+    }
+    finally {
+        session.endSession();
     }
 }
